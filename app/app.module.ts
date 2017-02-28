f304f8d257fa13b695f5d6fc12938166c320c5d1
NickChlam <Nick.Chlam@rht.com>
2017-02-27 23:52:08 -0500

    Add card.component.css .html .ts import to app.module.ts

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,12 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CardComponent } from './card/card.component';
 
 
 @NgModule({
     imports: [ BrowserModule ],
     declarations: [ AppComponent ],
-    bootstrap: [ AppComponent]
+    bootstrap: [ 
+        AppComponent,
+        CardComponent
+        ]
 })
 
 export class AppModule { }
